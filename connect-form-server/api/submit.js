dde3e965476a72948c6df9afb02320f2206ff5dd
Brian DeLorenzo <deloreb2@tcnj.edu>
2025-01-31 14:13:31 -0500

    Fix API deployment

--- connect-form-server/api/submit.js
+++ connect-form-server/api/submit.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
     // Allow CORS
     res.setHeader("Access-Control-Allow-Origin", "*"); // Allow all domains for now
     res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
